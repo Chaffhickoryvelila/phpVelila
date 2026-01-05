d445e5eebead27407279011eac89ed4c8737adbe
Chaffhickoryvelila <chaffvelila@gmail.com>
2026-01-05 09:02:29 +0800

    new login and css

---
+++ PAGES/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chaff Hickory Velila</title>
+    <link rel="stylesheet" href="../CSS/Login.css">
+</head>
+<body>
+    <h1>Login</h1>
+    <form action="">
+        <label for="username">Username</label>
+        <Input type="text" id= "username" class = "username" name="username">
+           <label for = "password" ="password">Password</label>
+        <Input type="password" id= "password" class = "password" name="password">
+        <button type = "submit"> Submit </button>
+    </form>
+    
+</body>
+</html>
