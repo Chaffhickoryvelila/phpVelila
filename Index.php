b9c4bbcdba2744bc81a73ef7315297dc00171214
Chaffhickoryvelila <chaffvelila@gmail.com>
2026-01-05 08:12:48 +0800

    first commit

---
+++ Index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1{
+            color: red;
+        }
+    </style>
+</head>
+<body>
+     <h1>Hello World</h1>
+
+       
+        <?php 
+      for($x = 0; $x<10; $x++){
+           echo("<h1>HELLO WORLD FROM PHP</h1>");
+      }
+     ?>
+
+
+    
+  
+</body>
+</html>
