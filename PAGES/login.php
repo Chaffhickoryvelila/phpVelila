4d38c74f1e12396e0cbd0fa2858c35de77c9112e
Chaffhickoryvelila <chaffvelila@gmail.com>
2026-01-06 09:54:24 +0800

    new changes

--- PAGES/login.php
+++ PAGES/login.php
@@ -1,20 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chaff Hickory Velila</title>
     <link rel="stylesheet" href="../CSS/Login.css">
 </head>
+
 <body>
-    <h1>Login</h1>
-    <form action="">
-        <label for="username">Username</label>
-        <Input type="text" id= "username" class = "username" name="username">
-           <label for = "password" ="password">Password</label>
-        <Input type="password" id= "password" class = "password" name="password">
-        <button type = "submit"> Submit </button>
-    </form>
-    
+
+    <div class="1">
+        <h1>Login</h1>
+    </div>
+
+    <div class="div2">
+        <form class="form">
+            <label for="username">Username</label>
+            <input type="text" id="username" class="username" name="username">
+
+            <label for="password">Password</label>
+            <input type="password" id="password" class="password" name="password">
+
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+
+
 </body>
+
 </html>
