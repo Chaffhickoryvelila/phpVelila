2e4ba962688bbf777f5d5726865971543275522f
Chaffhickoryvelila <chaffvelila@gmail.com>
2026-01-08 09:05:21 +0800

    new changes

--- PAGES/Dashboard.php
+++ PAGES/Dashboard.php
@@ -1,3 +1,10 @@
+<?php
+$name = $_GET['name'];
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,10 +12,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+     <link rel="stylesheet" href="../CSS/Dashboard.css">
+     
 </head>
 
 <body>
-    <h1> "Welcome to the page" </h1>
+     <!-- 🔹 HEADER / NAVBAR -->
+  <header class="operator">
+    <h1 class="h1s">
+      <span style="font-weight:bold;color: red;">C</span>H<span style="font-weight:bold;color: red;">A</span>FF
+      HIC<span style="font-weight:bold;color: red;">K</span>ORY
+      <span style="font-weight:bold;color: red;">V</span>EL<span style="font-weight:bold;color: red;">I</span>LA
+    </h1>
+
+    <div class="nav-text">
+      <span><a href="#">Home</a></span>
+      <span><a href="#about">About</a></span>
+      <span><a href="#projects">Projects</a></span>
+      <span><a href="#contacts">Contacts</a></span>
+    </div>
+  </header>
+
+    <h1> Welcome <?= $name ?> Have a nice day! </h1>
+    
+
 </body>
 
 </html>
--- PAGES/FUNCTIONS/LoginFunction.php
+++ PAGES/FUNCTIONS/LoginFunction.php
@@ -6,11 +6,12 @@
 
        $my_uname = "admin";
        $my_pass = "password";
+       $user_name = "Chaff Hickory Velila";
 
        if($username == $my_uname && $password == $my_pass){
-        echo "<script> window .location.href = '../Dashboard.php'</script>";
+        echo "<script> window .location.href = '../Dashboard.php?name=".$user_name."'</script>";
        }else{
-         echo "<script> window .location.href = '../login.php' </script>";
+         echo "<script> window .location.href = '../login.php?status=false ' </script>";
        }
 
           echo " Your username is " . $username . " and your password is " . $password;
