78e7bfa2752fe9c63c6084586809b7ef8b072993
Chaffhickoryvelila <chaffvelila@gmail.com>
2026-01-14 08:55:22 +0800

    new update

--- PAGES/Retrievepage.php
+++ PAGES/Retrievepage.php
@@ -1,15 +1,60 @@
+<?php
+$list = ['Serenity', 'Chaff', 'Jeni', 'Kelay', 'Yuri', 'Johnrave', 'Macy'];
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <?php
-      include "resources/header.php";
+include "resources/header.php";
 ?>
+
 <body>
-    <?php 
-     include "RESOURCES/Navigation.php";
+    <?php
+    include "RESOURCES/Navigation.php";
     ?>
 
-    <!--Contents --> 
+    <!--Contents -->
 
     <h1>This is Retrieve Page</h1>
+
+    <ul>
+        <?php
+        foreach ($list as $item) {
+
+        ?>
+
+            <li> <?= $item ?> </li>
+
+
+        <?php
+        }
+        ?>
+    </ul>
+
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Action</th>
+        </tr>
+
+        <?php
+        foreach ($list as $item) {
+
+        ?>
+
+            <tr>
+                <td> <?= $item ?> </td>
+                <td> <button>View</button> </td>
+            </tr>
+
+
+        <?php
+        }
+        ?>
+
+    </table>
+
 </body>
+
 </html>
