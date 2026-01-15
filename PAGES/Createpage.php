dea45eafd4d7883eaa5aa270a3387190319088ff
Chaffhickoryvelila <chaffvelila@gmail.com>
2026-01-15 09:08:58 +0800

    new power

--- PAGES/Createpage.php
+++ PAGES/Createpage.php
@@ -10,6 +10,20 @@
 
     <!--Contents --> 
 
-    <h1>This is Create Page</h1>
+    <div> 
+
+    <h1>Create User</h1>
+    <form action="FUNCTIONS/Createuser.php" method="POST">
+        <label for="">Name</label>
+        <input type="text" name = "user_name">
+        <label for="">Age</label>
+        <input type="number" name = "user_age">
+        <label for="">Address</label>
+        <input type="text" name = "user_address">
+
+        <button>Submit</button>
+
+    </form>
+    </div>
 </body>
 </html>
---
+++ PAGES/FUNCTIONS/Createuser.php
@@ -0,0 +1,7 @@
+<?php
+include "../../CONFIG/includes.php";
+
+if(isset($_POST['user_name'])&& isset($_POST['user_age']) && isset ($_POST['user_address'])){
+    echo "ready to save";
+}
+?>
